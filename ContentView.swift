551c40f1ab026f5ae46418e7f754695d7bf98130
627386 <asorathiya7386@stu.d214.org>
2025-09-08 09:14:41 -0700

    fix

--- ContentView.swift
+++ ContentView.swift
@@ -3,14 +3,11 @@ import SwiftUI
     
 struct ContentView: View {
     @State var tasks: [String] = ["Mow the lawn", "Buy Groceries", "Walk the Dog"]
-    @State var Chores: [Task] = [
-           Task(nameOfTask: "Mow the lawn"),
-           Task(nameOfTask: "Buy Groceries"),
-           Task(nameOfTask: "Walk the Dog")
-       ]
+    @State var nameOftask: [Task] = []
     var body: some View {
        List(tasks, id: \.self) { task in
             Text(task)
         }
     }
 }
+
