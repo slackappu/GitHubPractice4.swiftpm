8a462a585f4be0f7cc2d30492c132371d83fe38b
627386 <asorathiya7386@stu.d214.org>
2025-09-08 09:05:04 -0700

    part 2

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
+    
 struct ContentView: View {
     @State var tasks: [String] = ["Mow the lawn", "Buy Groceries", "Walk the Dog"]
+    @State var Chores: [Task] = [
+           Task(nameOfTask: "Mow the lawn"),
+           Task(nameOfTask: "Buy Groceries"),
+           Task(nameOfTask: "Walk the Dog")
+       ]
     var body: some View {
        List(tasks, id: \.self) { task in
             Text(task)
